52fbf3147774760212e85d9ee87a82fd47783941
rahuldcosta <rahuldc999@gmail.com>
2015-09-03 14:19:46 +0530

    Update foodieHome.php

--- application/controllers/foodieHome.php
+++ application/controllers/foodieHome.php
@@ -11,7 +11,7 @@ function __construct(){
 	
 	public function index()
         {
-            //Home
+            //Home page
             $this->load->view('FoodiePublicHome');
         }
         
