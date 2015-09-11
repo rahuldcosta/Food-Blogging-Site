c6294bec4d5cb60581fcf6723ff2b2dc72cf794b
MiracleT <talitadsouza130@gmail.com>
2015-09-11 21:41:11 +0530

    added search page

---
+++ application/views/searchSite.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<body>
+   
+<div id="sidebar" class="col-lg-9">
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">
+                        <strong>Searching made easy...</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div>
+                    
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+</div>
+
+    </div>
+    <!-- /.container -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; Foodie 2014</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url(); ?>/resources/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo base_url(); ?>/resources/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
