79554673e627c1c7e300efaa40c0236fb3c13b2e
MiracleT <talitadsouza130@gmail.com>
2015-09-12 21:34:03 +0530

    added UserPage

---
+++ application/views/userPage.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+     
+    </head>
+
+
+<body>
+   
+<div id="sidebar" class="col-lg-9">
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                     <div class="col-lg-10">
+                    <hr>
+                    <h2 class="intro-text text-center">Welcome
+                        <strong>Miracle(username)</strong>
+                  
+                <!--    <img class="img-responsive img-border-right"  style="height:5em;float:right;margin-top: -2em" src="<?php echo base_url(); ?>/resources/img/me.jpg" alt="">-->
+                   </h2> <hr>
+                     </div>
+                    <div class="col-lg-2">
+                        <img class="img-responsive img-border-right"  style="height:6em;float:right" src="<?php echo base_url(); ?>/resources/img/me.jpg" alt="">
+                    </div>
+                </div>
+              
+            <div >
+                <div>
+                <a   style="cursor:pointer ;margin-left:3%" title="edit profile">Edit Profile</a>
+                <a   style="cursor:pointer ;margin-left:3%" title="post">Post recipe</a>
+                <a   style="cursor:pointer ;margin-left:3%" title="edit">Edit recipe</a>
+                <a   style="cursor:pointer ;margin-left:3%" title="delete">Delete recipe</a>
+                 <a  style="cursor:pointer ;margin-left:3%" title="favorite">Favorite(0)</a> <!pinned interests-->
+                 <a   style="cursor:pointer ;margin-left:3%" title="Notifications">notifications(0)</a> <!whether your post accepted or rejected by admin-->
+                 <a style="cursor:pointer ;margin-left:3%" title="Credits">Credits(0)</a><!winning status-->
+                </div>
+                
+                <div style="margin-top:10em;margin-left:5em;margin-right:5em; text-align: center">
+                    You have no pinned lists yet.Edit your profile to add to  add to your interests!!
+                </div>
+	<div></div>
+</div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+</div>
+
+    </div>
+    <!-- /.container -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; Foodie 2014</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url(); ?>/resources/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo base_url(); ?>/resources/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
