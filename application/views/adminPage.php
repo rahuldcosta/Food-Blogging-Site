57fd6b6ece1b56f42ad55882f4b9190edf487c5d
MiracleT <talitadsouza130@gmail.com>
2015-09-13 18:32:47 +0530

    adminPage-restructured

--- application/views/adminPage.php
+++ application/views/adminPage.php
@@ -11,31 +11,49 @@
         <div class="row">
             <div class="box">
                 <div class="col-lg-12">
-                     <div class="col-lg-10">
+                     <div class="col-lg-9">
                     <hr>
-                    <h2 class="intro-text text-center">Welcome
-                        <strong>Talita(Admin)</strong>
+                    <h2 class="intro-text text-center">Welcome!!
+                      
                   
+                <!--    <img class="img-responsive img-border-right"  style="height:5em;float:right;margin-top: -2em" src="<?php echo base_url(); ?>/resources/img/me.jpg" alt="">-->
                    </h2> <hr>
+                     <strong>Task:</strong>
+                    <div style="margin-top:10em;margin-left:5em;margin-right:5em; text-align: center">
+                    You have not chosen any lists yet.Go to Manage Lists!!
+                </div>
                      </div>
-                    <div class="col-lg-2">
-                        <img class="img-responsive img-border-right"  style="height:6em;float:right" src="<?php echo base_url(); ?>/resources/img/admin.jpg" alt="">
+                    <div class="col-lg-3" style="border-left:2px dotted red;">
+                        <div>Admin: Talita Dsouza</div>
+                        <img class="img-responsive img-border-right"  style="height:9em;float:right;width:12em" src="<?php echo base_url(); ?>/resources/img/admin.jpg" alt="">
+                        <div style="border-bottom:2px dotted red;"> <br><a   style="cursor:pointer ;margin-left:3%" title="edit profile">Edit Profile</a>
+                            <br> <a   style="cursor:pointer ;margin-left:3%" title="edit profile">Change Password</a>
+                            <br>  <a   style="cursor:pointer ;margin-left:3%" title="edit profile">Logout</a>
+                    </div>
+                        <div style="border-bottom:2px dotted red;width:14em ;float:left; ;float:left;margin-left:0em">
+                            <div class=" panel-default">
+      <div>
+        <h4> <a data-toggle="collapse" data-parent="#accordion" href="#collapse6">Manage Lists</a>
+        </h4>
+      </div>
+      <div id="collapse6" class="panel-collapse collapse" >
+        <div class="panel-body" style="margin-left:-1em"><ul class="list-group">
+          <li class="list-group-item"><a href="GreenList">Manage GreenList(8)</a></li>
+          <li class="list-group-item"><a href="Blacklist">Manage BlackList(2)</a></li>
+          <li class="list-group-item"><a href="requests">Manage Requests(5)</a></li>
+          <li class="list-group-item"><a href="requests">Winner Lists</a></li>
+        </ul>
+         </div>
+    </div>
+      </div>
+                        </div>
                     </div>
                 </div>
               
             <div >
-                <div>
-                <a   style="cursor:pointer ;margin-left:3%" title="edit profile">Edit Profile</a>
-                <a   style="cursor:pointer ;margin-left:3%" title="post">Post Requests(2)</a><!users submiited recipes for admin verification ->
-                <a   style="cursor:pointer ;margin-left:3%" title="edit">Reject List(3)</a>
-                <a   style="cursor:pointer ;margin-left:3%" title="delete">Green List(50)</a>
-                 <a  style="cursor:pointer ;margin-left:3%" title="favorite">Monthly Results(0)</a> <!list of winners to be published end of month-->
-                 <a   style="cursor:pointer ;margin-left:3%" title="Notifications">notifications(0)</a> <!whether user successfully notified or no about recipe being accepted or rejected-->
-                </div>
+              
                 
-                <div style="margin-top:10em;margin-left:5em;margin-right:5em; text-align: center">
-                    You have 2 post requests.View and scrutinize them!!
-                </div>
+               
 	<div></div>
 </div>
                 <div class="clearfix"></div>
